ded2ebd1b6ce4b4c9094003f33f104bc605fea62
Yuriy Ustushenko <yoreek@yahoo.com>
2016-11-05 01:34:08 +0200

    db

---
+++ app/models/micropost.rb
@@ -0,0 +1,6 @@
+# Micropost
+class Micropost < ActiveRecord::Base
+  belongs_to :user
+  validates :content, length: { maximum: 140 },
+                      presence: true
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+# User
+class User < ActiveRecord::Base
+  has_many :microposts
+  validates :name, presence: true
+  validates :email, presence: true
+end
